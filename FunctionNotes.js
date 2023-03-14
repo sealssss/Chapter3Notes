2616b46ce05e2d1aff2d2220589692e2aec2914a
iuf7 <iuf7@fresnou.org>
2023-03-14 13:57:36 -0700

    initial commit

---
+++ FunctionNotes.js
@@ -0,0 +1,41 @@
+//Function Notes
+//Step 1 - Define the function
+function sayHello(){
+    console.log("Hello!");
+}
+//Step 2 - Call (use) the function
+/*sayHello();
+sayHello();
+sayHello();
+sayHello();*/
+
+/*
+function printMax(){
+    let x = 6;
+    let y = 29;
+    if (x>y){
+        console.log(`The larger number is ${x}`);
+    } else {
+        console.log(`The larger number is ${y}`);
+    }
+}
+
+printMax();*/
+
+function printMax(x, y){
+    if (x>y);{
+        console.log(`the larger number is ${x}`);
+    } else {
+        console.log(`the larger number is ${y}`);
+    }
+}
+// printMax(8,21);
+// printMax(8,-21);
+// printMax(2,2);
+// printMax(-8,21);
+
+function sum(number1, number2){
+    {
+    console.log(`${Number1} + ${Number2} = ${number1+number2}`);
+
+
