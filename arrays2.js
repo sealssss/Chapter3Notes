6e6eb78f0acd92e934a39db14ee7dbd57fbad288
iuf7 <iuf7@fresnou.org>
2023-03-29 14:15:33 -0700

    initial commit

--- arrays2.js
+++ arrays2.js
@@ -1,10 +1,52 @@
 //1
-function count(n) {
-    let myArr=[1,2];
-    for (let i = 2; i <= n; i++){
-        console.log(i);
+function countN(arr){
+    for (let i = 1; i < arr.length; i=i+6){
+        console.log(arr[i]);
     }
-    console.log(myArr);
 }
 
+countN( [1, 2]);
+
+// function count(n) {
+//     let myArr=[1,2];
+//     let i=arr.length;
+//     for (let i = 2; i <= n; i++){
+//         console.log(i);
+//     }
+//     console.log(myArr);
+// }
+// count([1, 2, 3, 4, 5, 6]);
+
 //2
+// function countN(arr) {
+//     for (let i = 1; i < arr.length; i=i+6) {
+//         console.log(arr[i]);
+//     }
+// }
+
+//3
+// function reaplceWithX(arr, x) {
+//     for (let i=0; i< arr.length; i++){
+//         arr[i]=x
+//     }
+//     console.log(arr);
+// }
+// replaceWithX([1,2,3,4], 7);
+
+//4
+function reaplceWithX(arr, x) {
+    for (let i=0; i< arr.length; i++){
+        arr[i]=x
+    }
+    console.log(arr);
+}
+replaceWithX([1,2,3,4], 7);
+
+//5
+function countN(arr){
+    for (let i = 1; i < arr.length; i=i+6){
+        console.log(arr[i]);
+    }
+}
+
+countN( [1, 2]);
