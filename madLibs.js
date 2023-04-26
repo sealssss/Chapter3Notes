5715c146f48abe0caf5c3eec6bbf5fdc8192ef9d
iuf7 <iuf7@fresnou.org>
2023-04-26 14:09:53 -0700

    initial commit

--- madLibs.js
+++ madLibs.js
@@ -10,9 +10,3 @@ const readLine=require('readline-sync');
 // arraysList.push(readline.question('Enter a verb:'));
 // arraysList.push(readLine.question('Enter a noun'));
 // console.log('arraysList has ${arraysList[0]} and ${arraysList[1]');
-
-readLine.question("How much ketchup packets do you want?");
-let answers =['one', 'two', 'three']
-let randomNum = Math.floor(Math.random()*answers.length);
-let answer = answers[randomNum];
-console.log(answer);
